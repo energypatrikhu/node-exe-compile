3925e75787661715e6230901a0ae5af85962c6b0
EnergyPatrikHU <energypatrikhu@proton.me>
2024-04-07 06:06:14 +0200

    Remove `__root` variable as it is not needed

--- build/esbuild.js
+++ build/esbuild.js
@@ -3,18 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.minify = void 0;
 const esbuild_1 = require("esbuild");
 const node_fs_1 = require("node:fs");
-const node_path_1 = require("node:path");
 const minify = async (entrypoint, destination) => {
-    const __root = (0, node_path_1.join)(require.main?.path || process.cwd());
-    const __destination = (0, node_path_1.join)(__root, destination);
-    const __packageJson = (0, node_path_1.join)(__root, 'package.json');
-    const __entrypoint = (0, node_path_1.join)(__root, entrypoint);
-    const packageJson = JSON.parse((0, node_fs_1.readFileSync)(__packageJson, 'utf-8'));
+    const packageJson = JSON.parse((0, node_fs_1.readFileSync)('package.json', 'utf-8'));
     await (0, esbuild_1.build)({
-        entryPoints: [__entrypoint],
+        entryPoints: [entrypoint],
         bundle: true,
         platform: 'node',
-        outdir: __destination,
+        outdir: destination,
         logLevel: 'debug',
         minify: true,
         format: 'cjs',
--- src/esbuild.ts
+++ src/esbuild.ts
@@ -1,20 +1,14 @@
 import { build } from 'esbuild';
 import { readFileSync } from 'node:fs';
-import { join } from 'node:path';
 
 export const minify = async (entrypoint: string, destination: string) => {
-	const __root = join(require.main?.path || process.cwd());
-	const __destination = join(__root, destination);
-	const __packageJson = join(__root, 'package.json');
-	const __entrypoint = join(__root, entrypoint);
-
-	const packageJson = JSON.parse(readFileSync(__packageJson, 'utf-8'));
+	const packageJson = JSON.parse(readFileSync('package.json', 'utf-8'));
 
 	await build({
-		entryPoints: [__entrypoint],
+		entryPoints: [entrypoint],
 		bundle: true,
 		platform: 'node',
-		outdir: __destination,
+		outdir: destination,
 		logLevel: 'debug',
 		minify: true,
 		format: 'cjs',
