4a024de93541f0633dfe9cb9cbfc3eeaed6b0bb9
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-09 22:21:10 +0200

    Update 'ora' import to use 'ora-classic' library

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,27 +1,45 @@
 import { build } from 'esbuild';
 import { existsSync, rmSync, readFileSync, writeFileSync } from 'node:fs';
 import { EOL } from 'node:os';
-import ora from 'ora';
+import ora from 'ora-classic';
+
+function oraStatus(text) {
+	const performanceNow = performance.now();
+	const spinner = ora(text).start();
+
+	return {
+		succeed: (text) => {
+			spinner.succeed(
+				`${text} in ${Math.round(
+					performance.now() - performanceNow,
+				)}ms`,
+			);
+		},
+		fail: (text) => {
+			spinner.fail(
+				`${text} in ${Math.round(
+					performance.now() - performanceNow,
+				)}ms`,
+			);
+		},
+	};
+}
 
 const __sourceFile = './src/index.ts';
 const __destinationFile = './build/index.js';
 const prefix = '#! /usr/bin/env node';
 
-const ora__removeOldFiles = ora('Removing old files...');
-const ora__building = ora('Building package...');
-const ora__addingShebang = ora('Adding shebang...');
-
-ora__removeOldFiles.start();
+const ora__removeOldFiles = oraStatus('Removing old files...');
 try {
 	if (existsSync(__destinationFile)) {
 		rmSync(__destinationFile, { force: true, recursive: true });
 	}
-	ora__removeOldFiles.succeed('Removed old files!');
+	ora__removeOldFiles.succeed('Removed old files');
 } catch {
-	ora__removeOldFiles.fail('Failed to remove old files!');
+	ora__removeOldFiles.failed('Failed to remove old files');
 }
 
-ora__building.start();
+const ora__building = oraStatus('Building package...');
 try {
 	const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
 	await build({
@@ -40,19 +58,19 @@ try {
 			...Object.keys(packageJson.dependencies || {}),
 		],
 	});
-	ora__building.succeed('Built package!');
+	ora__building.succeed('Built package');
 } catch {
-	ora__building.fail('Failed to build package!');
+	ora__building.failed('Failed to build package');
 }
 
-ora__addingShebang.start();
+const ora__addingShebang = oraStatus('Adding shebang...');
 try {
 	writeFileSync(
 		__destinationFile,
 		prefix + EOL + readFileSync(__destinationFile, 'utf-8'),
 		'utf-8',
 	);
-	ora__addingShebang.succeed('Added shebang!');
+	ora__addingShebang.succeed('Added shebang');
 } catch {
-	ora__addingShebang.fail('Failed to add shebang!');
+	ora__addingShebang.failed('Failed to add shebang');
 }
