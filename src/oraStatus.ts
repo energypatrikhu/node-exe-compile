b1c87a6acc89c17cac33aab53d895ef5bec5510f
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-09 21:41:50 +0200

    Add oraStatus function for displaying progress

---
+++ src/oraStatus.ts
@@ -0,0 +1,37 @@
+import ora from 'ora';
+
+export default function oraStatus(text: string) {
+	const performanceNow = performance.now();
+	const spinner = ora(text).start();
+
+	return {
+		succeed: (text: string) => {
+			spinner.succeed(
+				`${text} in ${Math.round(
+					performance.now() - performanceNow,
+				)}ms`,
+			);
+		},
+		fail: (text: string) => {
+			spinner.fail(
+				`${text} in ${Math.round(
+					performance.now() - performanceNow,
+				)}ms`,
+			);
+		},
+		info: (text: string) => {
+			spinner.info(
+				`${text} in ${Math.round(
+					performance.now() - performanceNow,
+				)}ms`,
+			);
+		},
+		warn: (text: string) => {
+			spinner.warn(
+				`${text} in ${Math.round(
+					performance.now() - performanceNow,
+				)}ms`,
+			);
+		},
+	};
+}
