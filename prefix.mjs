a3151e7b59b7648ec11757d09ba5e3cb849fae36
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-13 18:21:24 +0200

    Remove shebang prefixer

--- prefix.mjs
+++
@@ -1,7 +0,0 @@
-import { readFileSync, writeFileSync } from 'node:fs';
-import { EOL } from 'node:os';
-
-const prefix = '#! /usr/bin/env node';
-const file = 'build/index.js';
-
-writeFileSync(file, prefix + EOL + readFileSync(file, 'utf-8'), 'utf-8');
