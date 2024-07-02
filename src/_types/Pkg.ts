9de2d82a6dd211416e34b9faf7e1f3dfccd28f3c
EnergyPatrikHU <energypatrikhu@proton.me>
2024-07-02 16:51:57 +0200

    Add ability to change esbuild options

--- src/_types/Pkg.ts
+++ src/_types/Pkg.ts
@@ -1,17 +1,20 @@
+import type { BuildOptions } from 'esbuild';
+
 export interface Pkg {
-	targets: Array<string>;
-	scripts: Array<string>;
-	assets: Array<string>;
-	outputPath: string;
-	additional: {
-		[key: string]: string | Array<string>;
-	};
+  targets: Array<string>;
+  scripts: Array<string>;
+  assets: Array<string>;
+  outputPath: string;
+  additional: {
+    [key: string]: string | Array<string>;
+  };
 }
 
 export interface PkgConfig {
-	name: string;
-	main: string;
-	bin: string;
-	autoCopy: boolean;
-	pkg: Pkg;
+  name: string;
+  main: string;
+  bin: string;
+  autoCopy: boolean;
+  esbuild: BuildOptions;
+  pkg: Pkg;
 }
